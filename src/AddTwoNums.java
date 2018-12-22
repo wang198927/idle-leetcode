98506e4afb113dd443b0ac020e18a6f796a70c11
daoming.wang_UIH-AI <daoming.wang@united-imaging.com>
2018-12-22 16:38:14 +0800

    fixed null node bug

--- src/AddTwoNums.java
+++ src/AddTwoNums.java
@@ -4,16 +4,22 @@ public class AddTwoNums {
         ListNode newNode = new ListNode(0);
         ListNode node1 = l1, node2 = l2, curNode = newNode;
         int carry = 0;
-        while (null != node1 && null !=node2){
-            int value = node1.val+ node2.val+carry;
+        while (null != node1 || null !=node2){
+            int value1 = (null != node1)?node1.val:0;
+            int value2 = (null != node2)?node2.val:0;
+            int value = value1+ value2+carry;
             carry = value>=10 ? 1:0;
             value = value % 10;
             curNode.next = new ListNode(value);
             curNode = curNode.next;
 
-            node1 = node1.next;
-            node2 = node2.next;
+            if(null != node1) node1 = node1.next;
+            if(null != node2) node2 = node2.next;
         }
+        if (carry > 0) {
+            curNode.next = new ListNode(carry);
+        }
+
 
         return  newNode.next;
     }
